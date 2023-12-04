7df98b9288ff0b0359cb941ab1f25f51bb50e4b0
AlexMusikyan <a.musikyan.2003@mail.ru>
2023-12-04 16:51:58 +0400

    V21

---
+++ main.py
@@ -0,0 +1,269 @@
+# print("Heloo python team")
+#__________________________________HOMEWORK-1______________________________________________________________________________________________________
+# length = float(input("Enter the length of the rectangle: "))
+# width = float(input("Enter the width of the rectangle: "))
+# area = length * width
+
+# print("The area of the rectangle is:", area)
+
+
+#___________________________________HOMWEORK-2______________________________________________________________________________________
+# r = int(input('enter radius'))
+# pi = 3.14
+
+# area = pi * r * r
+
+# print('area of circl is' , area)
+
+#_________________________________HOMEWORK-3___________________________________________________________________________________________
+
+
+# country = "Armenia"  
+# city = "Paris"   
+# language = "Python"  
+# number = 42   
+# temperature = 25.5  
+# is_sunny = True  
+# is_weekend = False 
+# fruit = "Banana"  
+# color = "Blue"  
+# animal = "Elephant"    
+# flower = "Rose"  
+# car = "Toyota"   
+# food = "Pizza" 
+# movie = "la casa de papel"  
+# book = "Harry Potter"  
+# singer = "miyagi"  
+# computer = "MacBook"   
+# sport = "fotball"  
+# school = "Harvard"  
+
+# print (type(country))
+# print (type(city))
+# print (type(number))
+# print (type(temperature))
+# print (type(is_sunny))
+# print (type(is_weekend))
+# print (type(fruit))
+# print (type(color))
+# print (type(animal))
+# print (type(flower))
+# print (type(car))
+# print (type(food))
+# print (type(movie))
+# print (type(book))
+# print (type(singer))
+# print (type(computer))
+# print (type(sport))
+# print (type(school))
+
+#_________________________________________________________________________________________________________________________________________________
+
+# 
+#_________________________________________________________________________________________________________________________________________________
+
+
+# name = input('what is your name :')
+
+# print('Hello, ' + name + '! Nice to meete you')
+
+#_________________________________________________________________________________________________________________________________________________
+#3
+# length = float(input("Enter the length of the room in meters: "))
+# width = float(input("Enter the width of the room in meters: "))
+
+#  = length * width
+
+# print("The area of the room is", area, "square meters.")
+
+
+#_________________________________________________________________________________________________________________________________________________
+
+# Constants for square feet to acres conversion
+#SQUARE_FEET_IN_ACRE = 43560
+
+# Ask the user for the length of the garden plot in feet
+#length_feet = float(input("Enter the length of the garden plot in feet: "))
+
+# Ask the user for the width of the garden plot in feet
+#width_feet = float(input("Enter the width of the garden plot in feet: "))
+
+# Calculate the area of the garden plot in square feet
+#area_square_feet = length_feet * width_feet
+
+# Convert the area to acres
+#area_acres = area_square_feet / SQUARE_FEET_IN_ACRE
+
+# Display the area of the garden plot in acres
+#print("The area of the garden plot is", area_acres, "acres.")
+
+
+#_________________________________________________________________________________________________________________________________________________
+#8
+# souvenir_weight = 75  
+# trinket_weight = 112 
+
+# num_souvenirs = int(input("Enter the number of souvenirs purchased: "))
+# num_trinkets = int(input("Enter the number of trinkets purchased: "))
+
+
+# total_weight = (num_souvenirs * souvenir_weight) + (num_trinkets * trinket_weight)
+
+
+# print("The total weight of the parcel is", total_weight, "grams.")
+
+
+#_________________________________________________________________________________________________________________________________________________
+
+#6
+
+# Ask the user for the amount of the restaurant order
+# order_amount = float(input("Enter the amount of the restaurant order: "))
+
+# # Define the tax rate for your region (e.g., as a decimal, 0.08 for 8% tax)
+# tax_rate = 0.08
+
+# # Calculate tax amount
+# tax_amount = order_amount * tax_rate
+
+# # Calculate tip amount (18% of the order value excluding tax)
+# tip_amount = (order_amount - tax_amount) * 0.18
+
+# # Calculate the total amount (order amount + tax + tip)
+# total_amount = order_amount + tax_amount + tip_amount
+
+# # Format and display the tax, tip amount, and total with the specified format
+# formatted_tax = "${:.2f}".format(tax_amount)
+# formatted_tip = "${:.2f}".format(tip_amount)
+# formatted_total = "${:.2f}".format(total_amount)
+
+# # Display the results with the requested format
+# print("Tax amount:   ", formatted_tax)
+# print("Tip amount:   ", formatted_tip)
+# print("Total amount: ", formatted_total)
+
+#_________________________________________________________________________________________________________________________________________________
+
+# _____________________________krknutun____________________________________________________________________
+
+#2
+# name = input('what is your name :')
+
+# print('Hello, '+ name + '! Nice to meete you' )
+
+#3
+# x = int(input('enter  leght :' ))
+# y = int(input('enter withd :'  ))
+
+# area = x+y
+
+# print('arean is ',  area)
+
+#4
+# leght = int (input(' enter the leght (ft); '))
+# withd = int(input('enter the withd (ft): '))
+
+# area = leght * withd 
+
+# area_acra = area / 43560
+
+# print(F"area is {area_acra}" )
+
+#5
+# little = int(input('Enter count of little bottles: '))
+# big = int(input('Enter count of big bottles: '))
+
+# price = little * 0.10 + big * 0.25
+
+# print(f'You get $ {price:.2f}')
+# print('You get $ %.2f' % price)
+# print('You get $ {:.2f}'.format(price))
+
+# 6
+# price = int(input('Cost of your meal: '))
+
+# tax = price * 0.20
+# tip = price * 0.18
+
+# total = price + tax + tip
+
+# print(f'Tax: $ {tax:.2f}')
+# print(f'Tip: $ {tip:.2f}')
+# print(f'You need to pay $ {total:.2f}')
+
+#7
+# n = int(input("enter n: " ))
+# sum = n * (n + 1) // 2
+
+# print(f'sum is {sum}')
+
+#8
+# a = int(input("enter count of fsouviner "))
+# b = int (input("enter count of trinketds "))
+
+# weight = a * 75 + b * 115
+ 
+# print (f"total weight is {weight} g ")
+
+#9
+# deposit = int(input('Enter your deposit: '))
+
+# print(f'1 tari: $ {deposit * pow(1.10 , 1):.2f}')
+# print(f'2 tari: $ {deposit *pow(1.10 , 2):.2f}')
+# print(f'3 tari: $ {deposit * pow(1.10 , 3):.2f}')
+# print(f'4 tari: $ {deposit * pow(1.10 , 4):.2f}')
+# print(f'5 tari: $ {deposit * pow(1.10 , 5):.2f}')
+# print(f'6 tari: $ {deposit * pow(1.10 , 6):.2f}')
+# print(f'7 tari: $ {deposit * pow(1.10 , 7):.2f}')
+# print(f'8 tari: $ {deposit * pow(1.10 , 8):.2f}')
+# print(f'9 tari: $ {deposit * pow(1.10 , 9):.2f}')
+# print(f'10 tari: $ {deposit * pow(1.10 , 10):.2f}')
+# print(f'11 tari: $ {deposit * pow(1.10 , 11):.2f}')
+# print(f'12 tari: $ {deposit * pow(1.10 , 12):.2f}')
+# print(f'13 tari: $ {deposit * pow(1.10 , 13):.2f}')
+# print(f'14 tari: $ {deposit * pow(1.10 , 14):.2f}')
+# print(f'15 tari: $ {deposit * pow(1.10 , 15):.2f}')
+# print(f'16 tari: $ {deposit * pow(1.10 , 16):.2f}')
+# print(f'17 tari: $ {deposit * pow(1.10 , 17):.2f}')
+# print(f'18 tari: $ {deposit * pow(1.10 , 18):.2f}')   
+
+# 33
+
+# Ask  user for three integers
+# num1 = int(input("Enter the first integer: "))
+# num2 = int(input("Enter the second integer: "))
+# num3 = int(input("Enter the third integer: "))
+
+# # Find the minimum and maximum values
+# minimum = min(num1, num2, num3)
+# maximum = max(num1, num2, num3)
+
+# # Calculate the remaining number
+# total = num1 + num2 + num3
+# middle = total - maximum - minimum
+
+# # Display the integers in ascending order
+# print("Integers in ascending order:", minimum, middle, maximum)
+
+
+
+
+
+
+
+
+# user_input = input("Enter an integer: ")
+
+# try:
+#     number = int(user_input)
+
+    
+#     if number % 2 == 0:
+#         print(f"{number} is even.")
+#     else:
+#         print(f"{number} is odd.")
+
+# except ValueError:
+#     print("Invalid input. Please enter a valid integer.")
+
+
